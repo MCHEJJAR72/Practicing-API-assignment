c7cf4518ac071c3074327de598a6c9abd49c6ebb
mohamed chejjar <mchejjar@yahoo.com>
2024-06-23 11:24:08 -0400

    first commit

---
+++ index.py
@@ -0,0 +1,24 @@
+GitHub API: Access repositories, issues, pull requests, and more. Great for understanding how to interact with RESTful APIs.
+
+GitHub API Documentation
+OpenWeatherMap API: Retrieve weather data for various locations. Helps in understanding how to work with API keys and parameters.
+
+OpenWeatherMap API Documentation
+REST Countries API: Get information about countries, including basic data, currencies, languages, and more.
+
+REST Countries API Documentation
+CoinGecko API: Access cryptocurrency data, including prices, market data, and historical data.
+
+CoinGecko API Documentation
+NASA API: Explore NASA's open APIs for accessing astronomy and space-related data, including images, Mars Rover photos, and more.
+
+NASA APIs
+PokeAPI: Retrieve data about Pokémon, including their abilities, types, and more. Fun for building Pokémon-related projects.
+
+PokeAPI Documentation
+The Dog API: Access images and information about different dog breeds.
+
+The Dog API Documentation
+News API: Retrieve headlines and news articles from various sources and blogs.
+
+News API Documentation
